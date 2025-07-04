8623a9cf970e24e5623940f99e071c447e44ded2
Nilava Kazal <nilava.kazal@hva.nl>
2025-07-04 22:48:37 +0200

    feat: add hints with input check and color

--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var random = new Random();
+            int numberToGuess = random.Next(1, 101);
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Welcome to the Guess the Number Game!");
+            Console.WriteLine("Guess the number between 1 and 100!");
+            Console.ResetColor();
+
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+                if (!int.TryParse(input, out int guessedNumber) || guessedNumber < 1 || guessedNumber > 100)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
+                }
+                else if (guessedNumber < numberToGuess)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (guessedNumber > numberToGuess)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("🎉 Congratulations! You've guessed the number!");
+                    break;
+                }
+                Console.ResetColor();
+
+                input = Console.ReadLine();
+            }
+
         }
     }
 }
--- obj/Debug/net5.0/GuessTheNumber.AssemblyInfo.cs
+++ obj/Debug/net5.0/GuessTheNumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3281ef847716bbe9e7bdf5163e493f259cebc948")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
