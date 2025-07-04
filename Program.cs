2b3a68c9ff3d4d591c50d23ea253b6897ef5c118
Nilava Kazal <nilava.kazal@hva.nl>
2025-07-05 00:52:32 +0200

    refactor: extract PlayGame logic into separate Game class

--- Program.cs
+++ Program.cs
@@ -1,80 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using GuessTheNumber;
 
-namespace GuessTheNumber
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var game = new Game();
+        var random = new Random();
+        List<int> scoreboard = [];
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("Welcome to the Guess the Number Game!");
+        Console.ResetColor();
+
+        bool playAgain = true;
+        while (playAgain)
         {
-            var random = new Random();
-            List<int> scoreboard = [];
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("Welcome to the Guess the Number Game!");
+            scoreboard.Add(game.PlayGame(random));
+
+            var top5 = scoreboard.OrderBy(x => x).Take(5)
+            .Select((score, index) => $"{index + 1}. {score} attempts");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($"⭐️HIGHSCORE⭐️\n{string.Join("\n", top5)}");
+
             Console.ResetColor();
-
-            bool playAgain = true;
-            while (playAgain)
-            {
-                scoreboard.Add(PlayGame(random));
-
-                var top5 = scoreboard.OrderBy(x => x).Take(5)
-                .Select((score, index) => $"{index + 1}. {score} attempts");
-
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine($"⭐️HIGHSCORE⭐️\n{string.Join("\n", top5)}");
-
-                Console.ResetColor();
-                Console.WriteLine("Want to play again? (y/n)");
-                string answer = Console.ReadLine().ToLower();
-                playAgain = answer == "y";
-            }
-
-            Console.WriteLine("Thanks for playing! Goodbye!");
+            Console.WriteLine("Want to play again? (y/n)");
+            string answer = Console.ReadLine().ToLower();
+            playAgain = answer == "y";
         }
 
-        static int PlayGame(Random random)
-        {
-            int numberToGuess = random.Next(1, 101);
-            int attempts = 0;
-
-            Console.WriteLine("Guess the number between 1 and 100!");
-
-            while (true)
-            {
-                Console.ResetColor();
-                string input = Console.ReadLine();
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-
-                if (int.TryParse(input, out int guessedNumber) && guessedNumber >= 1 && guessedNumber <= 100)
-                {
-                    attempts++;
-
-                    if (guessedNumber < numberToGuess)
-                    {
-                        Console.WriteLine("Too low! Try again.");
-                    }
-                    else if (guessedNumber > numberToGuess)
-                    {
-                        Console.WriteLine("Too high! Try again.");
-                    }
-                    else if (guessedNumber == numberToGuess)
-                    {
-                        Console.WriteLine($"🎉 Congratulations! You've guessed the number with {attempts} attempts!");
-                        break;
-                    }
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
-                }
-
-                Console.ResetColor();
-
-            }
-            return attempts;
-        }
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
--- obj/Debug/net9.0/GuessTheNumber.AssemblyInfo.cs
+++ obj/Debug/net9.0/GuessTheNumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdfc9119e045515b6b481414d63001b99ef8dce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39cc03cd41537a3570bae9cf9f6b046a678a1ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessTheNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
