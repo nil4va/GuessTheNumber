218381dd47ac9806c5666373ad441755572bdba7
Nilava Kazal <nilava.kazal@hva.nl>
2025-07-04 23:17:06 +0200

    feat: add amount of attemps

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ namespace GuessTheNumber
         {
             var random = new Random();
             int numberToGuess = random.Next(1, 101);
+            int attempts = 0;
 
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Welcome to the Guess the Number Game!");
             Console.WriteLine("Guess the number between 1 and 100!");
             Console.ResetColor();
+
 
             string input = Console.ReadLine();
 
@@ -20,26 +22,33 @@ namespace GuessTheNumber
             {
                 Console.ForegroundColor = ConsoleColor.DarkMagenta;
 
-                if (!int.TryParse(input, out int guessedNumber) || guessedNumber < 1 || guessedNumber > 100)
+                if (int.TryParse(input, out int guessedNumber) && guessedNumber > 1 && guessedNumber < 100)
                 {
-                    Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
-                }
-                else if (guessedNumber < numberToGuess)
-                {
-                    Console.WriteLine("Too low! Try again.");
-                }
-                else if (guessedNumber > numberToGuess)
-                {
-                    Console.WriteLine("Too high! Try again.");
+                    attempts++;
+
+                    if (guessedNumber < numberToGuess)
+                    {
+                        Console.WriteLine("Too low! Try again.");
+                    }
+                    else if (guessedNumber > numberToGuess)
+                    {
+                        Console.WriteLine("Too high! Try again.");
+                    }
+                    else if (guessedNumber == numberToGuess)
+                    {
+                        Console.WriteLine($"🎉 Congratulations! You've guessed the number with {attempts} attempts!");
+                        break;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("🎉 Congratulations! You've guessed the number!");
-                    break;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
                 }
-                Console.ResetColor();
 
+                Console.ResetColor();
                 input = Console.ReadLine();
+
             }
 
         }
